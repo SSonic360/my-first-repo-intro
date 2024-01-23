eaa7f6b4ac276b01c05d4a1a3e551268ace31dc5
SSonic360 <juanmanuelgarcia496@gmail.com>
2024-01-22 21:43:35 -0500

    Added CSS and JavaScript code

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+const menuSection = $(".menu");
+const locationSection = $(".location");
+const hoursSection = $(".hours");
+
+menuSection.fadeIn(4000);
+locationSection.fadeIn(5000);
+hoursSection.fadeIn(5500);
